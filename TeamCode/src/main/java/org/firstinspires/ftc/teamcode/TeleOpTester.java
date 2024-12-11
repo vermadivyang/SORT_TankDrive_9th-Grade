717adb9bab2d4e45a22792060076cd8c0ab3e9ad
Divyang Verma <divyang.verma26@gmail.com>
2024-12-11 10:28:09 -0500

    Update and rename TeleOpTester to TeleOpTester.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTester.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="TeleOp | Tank")
+public class teleopMaize_Red extends OpMode {
+  
+    double forward=0;
+    double strafe=0;
+    double rotate=0;
+    public double MAXSPEED = .75;
+    TeleOpTank d = new TeleOpTank();
+
+    public void init(){
+        d.init(hardwareMap);
+
+      telemetry.addLine("init complete");
+      telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+       
+        forward=gamepad1.left_stick_y*-MAXSPEED;
+        rotate = -gamepad1.right_stick_x*MAXSPEED;
+        strafe = gamepad1.left_trigger*MAXSPEED-gamepad1.right_trigger*MAXSPEED;
+
+        d.driveTank(forward,rotate,strafe);
+
+
+        //Turtle Mode
+        if (gamepad1.a && !a_prev) {
+            if(!a_state) {
+                MAXSPEED = 0.3;
+                a_state = true;
+            } else {
+                MAXSPEED = 0.85;
+                a_state = false;
+            }
+        }
+
+        telemetry.addData("lift",gamepad2.right_stick_y);
+        telemetry.update();
+
+    }
+}
